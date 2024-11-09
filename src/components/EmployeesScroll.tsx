d32d55fc0a58c8a33eacdc866ce3661798b96f46
SimoneFerraris <79935593+SimoneFerraris@users.noreply.github.com>
2024-11-09 01:57:27 +0100

    dashboard design wip

--- src/components/EmployeesScroll.tsx
+++ src/components/EmployeesScroll.tsx
@@ -18,9 +18,9 @@ const Employee = (props: { empId: string; name: string; surname: string; lostMop
 const CartsScroll = (props: {type:string;}) => {
     const {type} = props;
     return (
-        <div className='bg-white w-full h-full rounded-bl-2xl rounded-r-md rounded-bt-md p-8 border border-blue-900'>
-            <div className='text-xl font-medium'>{props.type}</div>
-            <div className='overflow-y-auto overscroll-y-contain space-y-5 h-full'>        
+        <div className='bg-white w-full h-full rounded-bl-2xl rounded-t-md rounded-br-md p-6'>
+            <div className='text-xl font-medium pb-4 tracking-wider'>{props.type}</div>
+            <div className='overflow-y-auto overscroll-y-contain space-y-5 h-[80%]'>        
                 <Employee empId='1' lostMopsNmb={2} name='Donna Mannaia' surname='Lama'/>
                 <Employee empId='1' lostMopsNmb={2} name='Donna Mannaia' surname='Lama'/>
                 <Employee empId='1' lostMopsNmb={2} name='Donna Mannaia' surname='Lama'/>
@@ -41,9 +41,9 @@ const CartsScroll = (props: {type:string;}) => {
 const EmployeesScroll = (props: {type:string;}) => {
     const {type} = props;
     return (
-        <div className='bg-white w-full h-full rounded-l-md rounded-r-2xl p-8 border border-blue-900'>
-            <div className='text-xl font-medium'>{props.type}</div>
-            <div className='overflow-y-auto overscroll-y-contain space-y-5 h-full'>
+        <div className='bg-white w-full h-full rounded-l-md rounded-r-2xl p-6 '>
+            <div className='text-xl font-medium pb-4 tracking-wider'>{props.type}</div>
+            <div className='overflow-y-auto overscroll-y-contain space-y-5 h-[90%]'>
                         
                 <Employee empId='1' lostMopsNmb={2} name='Donna Mannaia' surname='Lama'/>
                 <Employee empId='1' lostMopsNmb={2} name='Donna Mannaia' surname='Lama'/>
--- src/components/InfoContainer.tsx
+++ src/components/InfoContainer.tsx
@@ -2,8 +2,13 @@
 const InfoContainer = () => {
     
     return (
-        <div className='bg-white w-full h-full rounded-tl-2xl rounded-bl-md rounded-r-md p-8 border border-blue-900 overflow-y-auto overscroll-y-contain space-y-5'>
-            <div className='text-xl font-medium'>Boh</div>
+        <div className='bg-[#c7c7c7] w-full h-full rounded-tl-2xl rounded-bl-md rounded-r-md p-8 border border-blue-900 overflow-y-auto overscroll-y-contain space-y-5 m-0 text-left'>
+            <div className='grid grid-cols-2'>
+                <div className='text-5xl font-semibold text-[#f25415]'>
+                    <h1>24</h1>
+                </div>
+            <div className='font-medium text-2xl text-[#777171] tracking-wider text-right'>MISSING MOPS</div>
+            </div>
         </div>  
     ) 
 }     
